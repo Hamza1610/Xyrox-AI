01c9bfb383bf41919c2f1d0069c71c35de8f847a
Hamza1610 <muhammadhamza162003@gmail.com>
2024-01-05 14:44:13 -0800

    added compute_text method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from engine import Engine
+from engine import Xyrox
 
 # Initializing engine class
-engine = Engine()
+engine = Xyrox()
 
 # Event variables
 START = True
@@ -9,19 +9,27 @@ EXIT = False
 LISTEN = True
 REPLY = False
 GEN_TEXT = ''
+GEMINI_GEN = ''
 
 # The welcome speech method welcome user to the appilcation
-
-engine.welcome_speech()
+# engine.welcome_speech()
 while START:
 
     # if LISTEN is True then call  record speech method and set LISTEN to False, REPLY TO True
     if LISTEN:
         #  # text generated from Speech recognition engine
-        # SR_GEN = engine.listen_to_speech()
-        SR_GEN = 'Hello how are you, are you in chat or generative'
-        # text generated from gemini engine
-        GEMINI_GEN = engine.compute_text(mode='chat', text= SR_GEN)
+        SR_GEN = engine.listen_to_speech()
+
+
+        GEMINI_GEN = engine.compute_text(mode='chat',text=SR_GEN)
+        # checks if SR_GEN is a local command
+        # iscommand = engine.iscommand(SR_GEN)
+        
+        # if iscommand == True:
+        #     EXIT = True
+        # else:
+        #     # text generated from gemini engine
+        #     GEMINI_GEN = engine.compute_text(mode='gen',text=SR_GEN)
 
         # State controller
         if GEMINI_GEN:
