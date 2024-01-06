4d20f9da97630fffe212a29c55747a5df7a42e14
Hamza1610 <muhammadhamza162003@gmail.com>
2024-01-06 02:26:56 -0800

    make Xyrox loop a class

--- main.py
+++ main.py
@@ -3,52 +3,63 @@ from engine import Xyrox
 # Initializing engine class
 engine = Xyrox()
 
-# Event variables
-START = True
-EXIT = False
-LISTEN = True
-REPLY = False
-GEN_TEXT = ''
-GEMINI_GEN = ''
 
-# The welcome speech method welcome user to the appilcation
-# engine.welcome_speech()
-while START:
+class Xyrox_loop:
 
-    # if LISTEN is True then call  record speech method and set LISTEN to False, REPLY TO True
-    if LISTEN:
-        #  # text generated from Speech recognition engine
-        SR_GEN = engine.listen_to_speech()
+    @staticmethod
+    def run():
 
-
-        GEMINI_GEN = engine.compute_text(mode='chat',text=SR_GEN)
-        # checks if SR_GEN is a local command
-        # iscommand = engine.iscommand(SR_GEN)
-        
-        # if iscommand == True:
-        #     EXIT = True
-        # else:
-        #     # text generated from gemini engine
-        #     GEMINI_GEN = engine.compute_text(mode='gen',text=SR_GEN)
-
-        # State controller
-        if GEMINI_GEN:
-            LISTEN = False
-            REPLY = True
-
-    # if REPLY is True then call  make speech method and set REPLY to False, LISTEN to True
-    if REPLY:
-        # calling engine method
-        engine.make_speech(GEMINI_GEN)
-
-        # State controller     
-        REPLY = False
+        # Event variables
+        START = True
+        EXIT = False
         LISTEN = True
+        REPLY = False
+        SR_GEN = ''
+        GEMINI_GEN = ''
 
-    # If Exit is True then Start is equals to False 
-    if EXIT:
+        engine.make_speech('Hello how are you doing today, how may I help you!')
+        # Xyrox loop
+        while START:
 
-        engine.exit_speech()
+            # if LISTEN is True then call  record speech method and set LISTEN to False, REPLY TO True
+            if LISTEN:
 
-        # State controller
-        START = False
+                while True:
+            
+                    #  # text generated from Speech recognition engine
+                    SR_GEN = engine.listen_to_speech()
+
+                    # Lenght of text genreated us 
+                    if SR_GEN:
+                        GEMINI_GEN = engine.compute_text(mode='chat',text=SR_GEN)
+                        break
+                    # checks if SR_GEN is a local command
+                    # iscommand = engine.iscommand(SR_GEN)
+                
+                    #if iscommand == True:
+                    #EXIT = True
+                    #else:
+                    # text generated from gemini engine
+                    #GEMINI_GEN = engine.compute_text(mode='gen',text=SR_GEN)
+
+                # State controller
+                if GEMINI_GEN:
+                    LISTEN = False
+                    REPLY = True
+
+            # if REPLY is True then call  make speech method and set REPLY to False, LISTEN to True
+            if REPLY:
+                # calling engine method
+                engine.make_speech(GEMINI_GEN)
+
+                # State controller     
+                REPLY = False
+                LISTEN = True
+
+            # If Exit is True then Start is equals to False 
+            if EXIT:
+
+                engine.exit_speech()
+
+                # State controller
+                START = False
