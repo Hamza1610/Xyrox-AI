1b0f383d6627577d293abaa67ce6a174b89f426f
Hamza1610 <muhammadhamza162003@gmail.com>
2024-01-05 17:25:25 -0800

    create and add welcome screen + Xyrox engine

---
+++ layouts/welcome_layout/welcome.py
@@ -0,0 +1,20 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+
+from engine import Xyrox
+
+
+engine = Xyrox()
+
+class WelcomeScreen(Screen, RelativeLayout):
+    Builder.load_file('layouts/welcome_layout/welcome.kv')
+
+    def __init__(self, **kwargs):
+        super(WelcomeScreen, self).__init__(**kwargs)
+
+    def on_press_start(self):
+        self.manager.current = 'chat'
+        engine.welcome_speech()
+        print("Button pressed!")
