f6643bee7bc277cfe970dc0dcc0d1ea85120034e
Hamza1610 <muhammadhamza162003@gmail.com>
2024-01-09 04:32:10 -0800

    added chat feature to gemini

--- google_config.py
+++ google_config.py
@@ -9,11 +9,48 @@ GOOGLE_API_KEY = os.getenv(key='GOOGLE_API_KEY')
 #configure gemini library
 genai.configure(api_key=GOOGLE_API_KEY)
 
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 512,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
 # Initialize Generative GEMINI Model
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-pro', generation_config=generation_config, safety_settings=safety_settings)
+
 
 # chat inititalize starting a chat with model 
-chat = model.start_chat(history=[])
+chat = model.start_chat(history=[
+    {
+        "role": "user",
+        "parts": ["Who are you?"]
+    },
+    {
+        "role": "model",
+        "parts": ["I'm Xyrox, your personal assistant, I do many things, I can create poems, make jokes, I  can gist and converse with you on any topic"]
+    },
+])
 
 def Gemini(mode,text):
     """_summary_ : The to to_markdown format gemini generated text to markdown format
